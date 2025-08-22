5f89a68970ba6d0f3886f648df89d1a42694edc3
Hugo Melin <hugo.melin01@gmail.com>
2025-08-22 11:17:54 +0200

    fix: added hashRouter for access to url like /project/torefi feat: added latest projects

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -18,7 +18,7 @@ const getImageUrl = (name: string) => {
       <div class="footer__adress">
         <h3>Mes contacts</h3>
         <address>
-          <a href="mailto:hugo.melin01@gmail.com">hugo.melin01@gmail.com</a>
+          <a href="mailto:hugo.melin@torefi.fr">hugo.melin@torefi.fr</a>
           <a href="tel:+0640059650">06.40.05.96.50</a>
           <p>La Rochelle 17000</p>
         </address>
@@ -43,7 +43,7 @@ const getImageUrl = (name: string) => {
       </div>
     </div>
     <div class="footer__copyright">
-      <p>Dernières modifications :<time datetime="2025-01-07">Janvier 2025</time></p>
+      <p>Dernières modifications :<time datetime="2025-22-08">Août 2025</time></p>
       <p class="copyright">{{ copyright }}</p>
     </div>
   </footer>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import AppHome from '@/views/App-Home.vue'
 import SingleProject from '@/views/Single-Project.vue'
 import NotFound from '@/views/Not-Found.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
